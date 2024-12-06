18f9a24b3619963388566a3ae6b549aff2b528db
Miruto80 <miguelfernando8012@gmail.com>
2024-12-06 12:07:05 -0400

    Arreglando

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function carga(){
 	let B =document.getElementById("Barra");
 	let h1 = document.getElementById("Titulo");
 	let body= document.getElementById("body");
+	let cartas = document.querySelectorAll(".car")
 	
 	b.onchange = function(){
 		if (this.checked){
@@ -11,12 +12,25 @@ function carga(){
 			h1.textContent = "Cleaning with Care";
 			h1.style.color="white";
 			body.style.background="#19e6e8";
+			cartas.forEach(carta => {
+                 carta.style.display="none";
+				 if(carta.id==="Cleaningcard"){
+					 carta.style.display="block";
+				 }
+			});
 		}
 		else{
 			B.style.background="#C3B6A7";
 			h1.textContent = "Moving Made Simple";
 			h1.style.color="#4A3C31";
 			body.style.background="#dcd5cc";
+			cartas.forEach(carta => {
+				carta.style.display="block";
+				if(carta.id==="Cleaningcard"){
+					carta.style.display="none";
+				}
+		   });
+			
 		}
 	}
 }
